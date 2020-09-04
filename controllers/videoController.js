0700dcc1c81886b082203826987c10e79827dbfa
KimYongKwon <dydrnjs518@gmail.com>
2020-09-04 17:09:33 +0900

    template Variable

--- controllers/videoController.js
+++ controllers/videoController.js
@@ -1,7 +1,7 @@
-export const home = (req, res) => res.render("Home");
-export const search = (req, res) => res.send("Search");
-export const videos = (req, res) => res.send("Videos");
-export const upload = (req, res) => res.send("Upload");
-export const videoDetail = (req, res) => res.send("Video Detail");
-export const editVideo = (req, res) => res.send("Edit Video");
-export const deleteVideo = (req, res) => res.send("Delete Video");
+export const home = (req, res) => res.render("home", {pageTitle: "Home"});
+export const search = (req, res) => res.render("Search", {pageTitle: "Search"});
+export const videos = (req, res) => res.render("Videos", {pageTitle: "videos"});
+export const upload = (req, res) => res.render("Upload", {pageTitle: "upload"});
+export const videoDetail = (req, res) => res.render("Video Detail", {pageTitle: "videoDetail"});
+export const editVideo = (req, res) => res.render("Edit Video", {pageTitle: "editVideo"});
+export const deleteVideo = (req, res) => res.render("Delete Video", {pageTitle: "deleteVideo"});
