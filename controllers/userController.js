3e067bf9bab62d067e8fa6d63b6cb3bd9a749209
KimYongKwon <dydrnjs518@gmail.com>
2020-09-06 21:27:28 +0900

    home controller

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,4 +1,25 @@
-export const join = (req, res) => res.render("join");
+import routes from "../routes";
+
+export const getJoin = (req, res) => {
+  res.render("join", { pageTitle: "Join" });
+};
+
+export const postJoin = (req, res) => {
+  console.log(req.body);
+  const {
+    body: { name, email, password, password2 },
+  } = req;
+  if (password !== password2) {
+    res.status(400);
+    res.render("join", { pageTitle: "Join" });
+  } else {
+    // to do: register user
+    // to do: Log user in
+    res.redirect(routes.home);
+  }
+  res.render("join", { pageTitle: "Join" });
+};
+
 export const login = (req, res) => res.render("login");
 export const logout = (req, res) => res.render("logout");
 export const users = (req, res) => res.render("users");
