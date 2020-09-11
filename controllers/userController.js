5d10c48f7a62a6ce91a6cf812d74286f3c1f08a0
KimYongKwon <dydrnjs518@gmail.com>
2020-09-11 13:43:31 +0900

    uploading and creating a video part two

--- controllers/userController.js
+++ controllers/userController.js
@@ -5,7 +5,6 @@ export const getJoin = (req, res) => {
 };
 
 export const postJoin = (req, res) => {
-  console.log(req.body);
   const {
     body: { name, email, password, password2 },
   } = req;
@@ -13,25 +12,26 @@ export const postJoin = (req, res) => {
     res.status(400);
     res.render("join", { pageTitle: "Join" });
   } else {
-    // to do: register user
-    // to do: Log user in
+    // To Do: Register User
+    // To Do: Log user in``
     res.redirect(routes.home);
   }
-  res.render("join", { pageTitle: "Join" });
 };
 
 export const getLogin = (req, res) =>
-  res.render("login", { pageTitle: "Log in" });
-
+  res.render("login", { pageTitle: "Log In" });
 export const postLogin = (req, res) => {
   res.redirect(routes.home);
 };
 
 export const logout = (req, res) => {
-  // res.render("logout");
+  // To Do: Process Log Out
   res.redirect(routes.home);
 };
-export const users = (req, res) => res.render("users");
-export const userDetail = (req, res) => res.render("userDetail");
-export const editProfile = (req, res) => res.render("editProfile");
-export const changePassword = (req, res) => res.render("changePassword");
+
+export const userDetail = (req, res) =>
+  res.render("userDetail", { pageTitle: "User Detail" });
+export const editProfile = (req, res) =>
+  res.render("editProfile", { pageTitle: "Edit Profile" });
+export const changePassword = (req, res) =>
+  res.render("changePassword", { pageTitle: "Change Password" });
--- controllers/videoController.js
+++ controllers/videoController.js
@@ -6,6 +6,7 @@ export const home = async (req, res) => {
     const videos = await Video.find({});
     res.render("home", { pageTitle: "Home", videos });
   } catch (error) {
+    console.log(error);
     res.render("home", { pageTitle: "Home", videos: [] });
   }
 };
@@ -20,12 +21,18 @@ export const search = (req, res) => {
 export const getUpload = (req, res) =>
   res.render("upload", { pageTitle: "Upload" });
 
-export const postUpload = (req, res) => {
+export const postUpload = async (req, res) => {
   const {
-    body: { file, title, description },
+    body: { title, description },
+    file: { path },
   } = req;
-  // To Do: upload and save video
-  res.redirect(routes.videoDetail(232323));
+  const newVideo = await Video.create({
+    fileUrl: path,
+    title,
+    description,
+  });
+  console.log(newVideo);
+  res.redirect(routes.videoDetail(newVideo.id));
 };
 
 export const videoDetail = (req, res) =>
