dd801f0902e7b627c9eda293f8d4b584b016a65c
KimYongKwon <dydrnjs518@gmail.com>
2020-10-08 22:13:33 +0900

    es6 with Webpack

--- webpack.config.js
+++ webpack.config.js
@@ -7,10 +7,18 @@ const ENTRY_FILE = path.resolve(__dirname, "assets", "js", "main.js");
 const OUTPUT_DIR = path.join(__dirname, "static");
 
 const config = {
-  entry: ENTRY_FILE,
+  entry: ["@babel/polyfill", ENTRY_FILE],
   mode: MODE,
   module: {
     rules: [
+      {
+        test: /\.(js)$/,
+        use: [
+          {
+            loader: "babel-loader",
+          },
+        ],
+      },
       {
         test: /\.(scss)$/,
         use: [
@@ -50,7 +58,7 @@ const config = {
     new MiniCssExtractPlugin({
       // Options similar to the same options in webpackOptions.output
       // both options are optional
-      filename: "[name].css",
+      filename: "styles.css",
     }),
   ],
 };
